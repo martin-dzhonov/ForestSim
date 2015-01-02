4e21c72d5e6e3a70df13b0963f186c4a80a0aea1
Martin Dzhonov <martin.dzhonov@gmail.com>
2015-01-02 21:53:13 +0200

    Initial

---
+++ ForestSim/ForestSim/WindowsSize.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ForestSim
+{
+    public enum WindowSize
+    {
+        Width = 1200,
+        Height = 600
+    }
+}
